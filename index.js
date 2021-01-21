e5d342236f308bde9b5cef416ee4ce98d70ee6b5
Max <hiomimi@gmail.com>
2021-01-21 20:06:04 +0300

    test

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const port = 2400;
 process.on('exit', () => app.listen(port, () => console.log(`Server was started with port ${port}`)));
 process.on('uncaughtException', () => app.listen(port, () => console.log(`Server was started with port ${port}`)))
 
-app.listen(port, () => console.log(`Server was started with port ${port}`))
+app.listen(port, () => console.log(`Server was started with port ${port}`));
+console.log('Testing roll backing of git');
