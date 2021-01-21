e1767be7828ca7f749021bfcb58db3ceda783a8c
Max <hiomimi@gmail.com>
2021-01-21 16:09:37 +0300

    basic things did to run our application of yoga site

---
+++ index.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+const port = 2400;
+
+process.on('exit', () => app.listen(port, () => console.log(`Server was started with port ${port}`)));
+process.on('uncaughtException', () => app.listen(port, () => console.log(`Server was started with port ${port}`)))
+
+app.listen(port, () => console.log(`Server was started with port ${port}`))
