2e8a3ffc93a035d442c283cc2e4e60d859d365a8
Max <hiomimi@gmail.com>
2021-01-21 20:25:55 +0300

    Revert "test"
    This reverts commit e5d342236f308bde9b5cef416ee4ce98d70ee6b5.

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const port = 2400;
 process.on('exit', () => app.listen(port, () => console.log(`Server was started with port ${port}`)));
 process.on('uncaughtException', () => app.listen(port, () => console.log(`Server was started with port ${port}`)))
 
-app.listen(port, () => console.log(`Server was started with port ${port}`));
-console.log('Testing roll backing of git');
+app.listen(port, () => console.log(`Server was started with port ${port}`))
